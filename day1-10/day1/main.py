2c641206bb10de7f7a3e3aeac2fa72e2267d2a4b
Manav Chaudhary <vrmanav02@gmail.com>
2023-06-20 10:01:45 +0530

    Day 1 ✓
    Band Name Generator

---
+++ day1-10/day1/main.py
@@ -0,0 +1,7 @@
+# Day 1 - Band Name Generator
+
+print("Welcome to Band Name Generator")
+city_name = input("Which city did you grow up in?\n")
+pet_name = input("What's the name of your pet?\n")
+band_name = city_name + " " + pet_name
+print(f"Your band name could be - {band_name}")
